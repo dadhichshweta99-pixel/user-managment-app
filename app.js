7e597bd9afe0147e4a8860bac8b553daab2d5657
shweta <ss.01dadhich@gmail.com>
2026-03-23 16:32:49 +0530

    Initial commit - User Managment App

---
+++ app.js
@@ -0,0 +1,117 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+
+const User = require('./models/User');
+
+const app = express();
+
+// Middleware
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(session({
+    secret: 'secretKey',
+    resave: false,
+    saveUninitialized: false
+}));
+
+// MongoDB Connection
+mongoose.connect('mongodb://127.0.0.1:27017/userApp')
+.then(() => console.log("DB Connected"))
+.catch(err => console.log(err));
+
+// Routes
+
+// Home
+app.get('/', (req, res) => {
+    res.redirect('/login');
+});
+
+// Signup Page
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+// Signup Logic
+const bcrypt = require('bcryptjs'); // add at top of file
+
+app.post('/signup', async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+        name,
+        email,
+        password: hashedPassword
+    });
+
+    await user.save();
+    res.redirect('/login');
+});
+
+// Login Page
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// Login Logic
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (isMatch) {
+            req.session.userId = user._id;
+            res.redirect('/dashboard');
+        } else {
+            res.send("Wrong password");
+        }
+    } else {
+        res.send("User not found");
+    }
+});
+
+// Dashboard (Protected)
+app.get('/dashboard', async (req, res) => {
+    if (!req.session.userId) return res.redirect('/login');
+
+    const users = await User.find();
+    res.render('dashboard', { users });
+});
+
+// Delete User
+app.get('/delete/:id', async (req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    res.redirect('/dashboard');
+});
+
+// Edit Page
+app.get('/edit/:id', async (req, res) => {
+    const user = await User.findById(req.params.id);
+    res.render('edit', { user });
+});
+
+// Update User
+app.post('/edit/:id', async (req, res) => {
+    const { name, email } = req.body;
+
+    await User.findByIdAndUpdate(req.params.id, { name, email });
+    res.redirect('/dashboard');
+});
+
+// Logout
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/login');
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
